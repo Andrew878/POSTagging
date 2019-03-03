3fd40224a12f7be56c7253a6ec71431635874407
al278 <al278@st-andrews.ac.uk>
2019-03-03 19:45:22 +0000

    viterbi: intialization matrix complete

--- venv/Viterbi.py
+++ venv/Viterbi.py
@@ -1,29 +1,61 @@
 from nltk import FreqDist, WittenBellProbDist
+from math import log, exp
 
 
-def viterbi_path(sent, freq_dist_tag_single_SMOOTH, freq_dist_tagWordPair_SMOOTH, freq_dist_tagBigram_SMOOTH,
-                 freq_dist_tag_single):
+def viterbi_path(sent, freq_dist_tag_single, freq_dist_tag_single_SMOOTH, freq_dist_tagWordPair_SMOOTH,
+                 freq_dist_tagBigram_SMOOTH,
+                 freq_dist_tagWordPairWithStartEnd_SMOOTH):
+
+    if(len(sent))
+
+
+    start = '<s>'
+    end = '</s>'
+
     prob_start = 1.0
     word_list = [w for (w, _) in sent]
-    start = '<s>'
 
-    # create path prob matrix. the  '-2' is to remove the start and end symbols
+    # create a set of states without start or end states
+    non_beg_or_end_states = set(freq_dist_tag_single.keys())
+    non_beg_or_end_states.remove(start)
+    non_beg_or_end_states.remove(end)
 
-    viterbi = [[]] * (len(word_list))
+    viterbi = [['' for x in range(len(non_beg_or_end_states))] for y in range(len(word_list))]
+    back_pointer = [['' for x in range(len(non_beg_or_end_states) + 1)] for y in range(len(word_list))]
 
-    set(sent)
+    print(viterbi)
 
-    i = 0
-    for state in freq_dist_tag_single.keys():
+    state_num = 0
+    for state in non_beg_or_end_states:
         to_from_tag_pair = (start, state)
         print("to_from_tag_pair =", to_from_tag_pair)
 
         word_given_tag_pair = (word_list[1], state)
         print("word_given_tag_pair = ", word_given_tag_pair)
 
-        a = freq_dist_tagBigram_SMOOTH.prob(to_from_pair) * prob_start
-        b = freq_dist_tagWordPair_SMOOTH(word_given_tag_pair)
+        a = log(freq_dist_tagBigram_SMOOTH.prob(to_from_tag_pair)) + log(prob_start)
+        b = log(freq_dist_tagWordPair_SMOOTH.prob(word_given_tag_pair))
 
-        viterbi[i][1] = a * b
+        if (state == start):
+            viterbi[0][state_num] = log(1)
 
-        print(viterbi[i][1])
+        viterbi[1][state_num] = a + b
+        back_pointer[1][state_num] = start
+        state_num += 1
+
+    print(viterbi[:][:])
+    print(back_pointer[:][:])
+
+    word_num = 1
+    for word in word_list[1:]:
+
+        state_num = 0
+        for state in non_beg_or_end_states:
+            viterbi[word_num][state_num] = viterbi[word_num-1][state_num]
+
+    return ()
+
+
+def best_path_given_back_pointers(back_pointer):
+    while (len(back_pointer) > 0):
+        be
